8a19ad787e5d889d0c01e76e5adb9664ec1aab07
Michael Ivanchenko <misha@kvvaiu.ru>
2020-03-08 22:48:34 +0300

    2020-03-08 init-method, destroy-method, factory-method

--- src/main/java/ru/imv/tutorial/spring/BeanApp.java
+++ src/main/java/ru/imv/tutorial/spring/BeanApp.java
@@ -7,17 +7,23 @@ public class BeanApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("app_context.xml");
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        Music classicalMusic = context.getBean("classicalBean", ClassicalMusic.class);
+        Music classicalMusic1 = context.getBean("classicalBean", ClassicalMusic.class);
 
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(comparison);
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
+        System.out.println("Playing0: " + classicalMusic.getSong());
+        System.out.println("Playing1: " + classicalMusic1.getSong());
 
-        firstMusicPlayer.setVolume(11);
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(comparison);
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        firstMusicPlayer.setVolume(11);
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
--- src/main/java/ru/imv/tutorial/spring/ClassicalMusic.java
+++ src/main/java/ru/imv/tutorial/spring/ClassicalMusic.java
@@ -1,8 +1,24 @@
 package ru.imv.tutorial.spring;
 
 public class ClassicalMusic implements Music {
+
+    private ClassicalMusic() {
+    }
+
+    public void initMusic() {
+        System.out.println("Doing Classical music init");
+    }
+
+    public void destroyMusic() {
+        System.out.println("Doing Classical music destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rapsody";
+    }
+
+    public static Music createMusic() {
+        return new ClassicalMusic();
     }
 }
